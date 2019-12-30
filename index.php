7fcbe0e24fd62c1289eb0d962d4ef85422aaa6db
nguyendvkgcs18074 <46696670+nguyendvkgcs18074@users.noreply.github.com>
2019-12-30 12:05:20 +0700

    new version

--- index.php
+++ index.php
@@ -5,3 +5,19 @@ function alert($msg) {
     echo "<script type='text/javascript'>alert('$msg');</script>";
 }
 ?>
+<html>
+<head>
+  <title>ATN Web Application</title>
+</head>
+  <body>
+    <link rel="stylesheet" href="D:\School\Cloud Computing\ATN-Web-Application\style.css">
+    <div class="hero-image">
+    <div class="hero-text">
+      <h1>Welcome To ATN Web Application</h1>
+      <p>This Application belongs to ATN Company</p>
+      <button>Get Started</button>
+    </div>
+  </div>
+
+    </div>
+  </body>
